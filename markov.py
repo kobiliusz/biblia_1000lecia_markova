1a3395f055f3a99e43d8a1f1e1634c490fc2d92e
kobiliusz <kobiliusz@gmail.com>
2021-06-12 11:20:01 +0200

    aktywacja konca swiata

--- markov.py
+++ markov.py
@@ -5,5 +5,5 @@ with open("cala_biblia", "r") as f:
 
 text_model = markovify.Text(text, state_size=3)
 
-for i in range(5):
+for i in range(666):
     print(text_model.make_sentence(min_words=10, tries=10000))
